7216fd1a28999a6b3f48bb1cfc1798e910784a31
IchsanCh <ichsanmuhammed01@gmail.com>
2025-08-03 09:49:08 +0700

    19

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -79,17 +79,6 @@
                                             </div>
                                             <span class="text-sm text-base-content/70">Integrasi API Sistem</span>
                                         </div>
-                                        <div class="flex items-center gap-3">
-                                            <div class="w-5 h-5 bg-success rounded-full flex items-center justify-center">
-                                                <svg class="w-3 h-3 text-success-content" fill="currentColor"
-                                                    viewBox="0 0 20 20">
-                                                    <path fill-rule="evenodd"
-                                                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                                        clip-rule="evenodd"></path>
-                                                </svg>
-                                            </div>
-                                            <span class="text-sm text-base-content/70">Support 24/7</span>
-                                        </div>
                                     </div>
 
                                     <!-- CTA Button -->
