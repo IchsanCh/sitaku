6f776eeef6685b512f952e1fe8b4e428189b403a
IchsanCh <ichsanmuhammed01@gmail.com>
2025-08-04 15:33:11 +0700

    fixing logic

--- resources/views/user/pegawai.blade.php
+++ resources/views/user/pegawai.blade.php
@@ -139,6 +139,18 @@
         </dialog>
 
         <div class="max-w-4xl mx-auto px-6 py-8 pt-4">
+            <div class="alert alert-info text-sm mb-3">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                </svg>
+                <span class="font-semibold">
+                    Mohon maaf, untuk saat ini maksimal satu pegawai per posisi.
+                    Jika ada lebih dari satu, silakan pilih salah satu saja. <span class="font-bold">Tidak boleh ada
+                        duplikasi posisi!!!</span>
+                </span>
+            </div>
             <div class="alert alert-warning text-sm mb-6">
                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-4 w-4" fill="none"
                     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -149,7 +161,6 @@
                     <span class="font-mono font-bold">Verifikasi</span>
                     bukan <span class="font-mono font-bold">verifikasi</span></span>
             </div>
-
             <div class="flex flex-col sm:flex-row gap-4 items-start sm:items-center justify-between mb-6">
                 <div class="flex-1 max-w-md">
                     <div class="form-control">
